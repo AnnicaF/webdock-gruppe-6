e86c164f481feb780c19fb6bebe282c773c198bc
Annica <annif011098@gmail.com>
2023-11-01 08:59:08 +0100

    tilføjelse af router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Feature        from '../views/Feature.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Feature',
+      component: Feature,
+    }
+  ]
+})
+
+export default router
