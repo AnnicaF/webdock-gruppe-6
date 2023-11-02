2a46ca66a0d52667a96111f6bb4ffd109f43ff0a
Artaey <95622783+Artaey@users.noreply.github.com>
2023-11-02 09:35:21 +0100

    still not working

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use("/api", require("./routes/api"));
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+}));
+
 
 // Start your server
-const PORT = 5173; // Use the correct port
+const PORT = 3005; // Use the correct port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/components/MakeRequest.vue
+++ src/components/MakeRequest.vue
@@ -5,31 +5,37 @@
 </template>
 
 <script>
+    console.log("a");
     export default {
         data() {
             return {
                 inputData: "",
             };
+            console.log("b");
         },
     methods: {
         async submitData() {
-        const response = await fetch("/api/insertData", {
+        const response = await fetch("http://localhost:3005/api/insertData", {
             method: "POST",
             headers: {
             "Content-Type": "application/json",
             },
             body: JSON.stringify({ data: this.inputData }),
         });
-
+        console.log("c");
         if (response.ok) {
             const data = await response.json();
             console.log("Data sent to the server:", data);
+            console.log("d");
         } else {
             console.error("Error sending data:", response.statusText);
+            console.log("e");
         }
+        console.log("f");
         },
     },
 };
+console.log("g");
 </script>
 
 <style>
