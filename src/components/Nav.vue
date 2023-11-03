1ec8f5ac285ef4cb36c5a075e766fc142d8eb911
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2023-11-03 11:10:42 +0100

    Added navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,22 @@
 <template>
 
-    <div>Hej Cecilie, Her skal du placere alt din html.</div>
+    <div>
+        <div id="top-nav">
+     <nav>
+        <div class="branding">
+         <a class="logo" alt="Webdock.io logo" href="/src/assets/images/webdock-logo-positiv.svg.png"></a>
+        </div>
+        <ul class="navbar-menu">
+         <li><a href="#Roadmap">Roadmap</a></li>
+         <li><a href="#Feature Request">Feature Request</a></li>
+        </ul>
+        <ul class="logsign">
+            <button class="signup">Sign up</button>
+            <button class="login">Login</button>
+        </ul>
+     </nav>
+   </div>
+    </div>
 
 </template>
 
