366fec789a09962f03b4525dfa23ff6df04fdd65
Artaey <95622783+Artaey@users.noreply.github.com>
2023-11-10 12:25:49 +0100

    done with frame work

--- src/components/MakeRequest.vue
+++ src/components/MakeRequest.vue
@@ -2,16 +2,27 @@
     <p>title</p>
     <input id="input" type="text">
     <button @click="submitData">Submit</button>
+    <br>
+    <button @click="getData">Get</button>
+    <ul>
+        <li v-for="request in requests" :key="request.id">{{request.title}}, {{request.bodyText}}</li>
+    </ul>
+    
 </template>
 
 <script setup>
+    import axios from "axios";
+    import {ref} from "vue";
+
+    const requests = ref(null);
+
     function submitData(){
         let data = {
             title: document.getElementById("input").value,
             bodyText: document.getElementById("input").value
         }; 
 
-        fetch('http://localhost:3000/api/v1/user', {
+        fetch('http://localhost:3000/api/v1/request', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json', // Set the content type to JSON
@@ -31,6 +42,15 @@
                 console.error('Error:', error);
             });
     }
+
+    axios.get('http://localhost:3000/api/v1/request')
+        
+        .then(response => requests.value = response.data)
+
+        .catch(err => {
+            console.log("error: " + err);
+        });
+
 </script>
 
 <style>
