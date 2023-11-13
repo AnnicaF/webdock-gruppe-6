1ce8a83baa2cc864ccdef999eefb0ee1e3b3f99c
Annica <annif011098@gmail.com>
2023-11-13 19:00:57 +0100

    delete some small mistake

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,24 @@
 <template>
-    <header>
-        <nav>
-            <div class="branding">
-                <img src="/src/assets/images/webdock-logo-full-whitebg.png" alt="Webdock.io logo">
-            </div>
-            <ul class="navigation">
-             <li><router-link :to="{ name: 'Feature' }">Feature request</router-link></li>
-             <li><router-link :to="{ name: 'Roadmap' }">Roadmap</router-link></li>
-             </ul>
-                <ul class="logsign">
-                    <button type="button" class="login">Login</button>
-                    <button type="button" class="signup">Sign up</button>
-                 </ul>
-        </nav>
-    </header>
+  <header>
+    <nav>
+      <div class="branding">
+        <img
+          src="/src/assets/images/webdock-logo-full-whitebg.png"
+          alt="Webdock.io logo"
+        />
+      </div>
+      <ul class="navigation">
+        <li>
+          <router-link :to="{ name: '' }">Feature request</router-link>
+        </li>
+        <li><router-link :to="{ name: '' }">Roadmap</router-link></li>
+      </ul>
+      <ul class="logsign">
+        <button type="button" class="login">Login</button>
+        <button type="button" class="signup">Sign up</button>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
-
-import { createRouter, createWebHistory } from 'vue-router'
-import Feature        from '../views/Feature.vue'
-import Roadmap from '@/views/Roadmap.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Feature from "../views/Feature.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,13 +9,6 @@ const router = createRouter({
       name: "Feature",
       component: Feature,
     },
-    {
-      path: '/roadmap',
-      name: 'Roadmap',
-      component: Roadmap,
-    },
-  ]
-})
   ],
 });
 
