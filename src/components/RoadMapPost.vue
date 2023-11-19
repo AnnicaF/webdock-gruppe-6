474651e6cd8f09dc5513f8f81c337326471cf0cd
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2023-11-19 11:06:30 +0100

    added hardcoded posts array for roadmap

---
+++ src/components/RoadMapPost.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <div class="roadmapSpacer"></div>
+    <div class="contentContainer">
+      <div class="contentInnerContainer">
+        <div class="roadmapContainer">
+          <div class="roadmapColumn">
+            <div class="roadmapHeader">
+              <h3 class="roadmapTitle">Planned</h3>
+            </div>
+            <div class="scrollContainer">
+              <div v-for="(post, index) in posts" :key="index">
+                <div class="postContainer">
+                  <div class="upvoteContainer">
+                    <p class="upvoteCount">{{ post.upvoteCount }}</p>
+                    <button
+                      @click="upvote(post)"
+                      class="upvote-button"
+                    ></button>
+                    <font-awesome-icon
+                      class="fa-lg"
+                      icon="fa-solid fa-caret-up"
+                    />
+                  </div>
+                  <h3>{{ post.title }}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: Array,
+  },
+  methods: {
+    upvote(post) {
+      post.upvoteCount += 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.contentContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.contentInnerContainer {
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+}
+
+.roadmapSpacer {
+  padding: 50px;
+}
+
+.roadmapContainer {
+  display: grid;
+  justify-items: start;
+  align-items: center;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px;
+  grid-auto-rows: auto;
+  padding: 10px;
+  justify-content: center;
+  align-content: center;
+}
+
+.roadmapColumn {
+  border: 1px solid gray;
+  border-radius: 10px;
+}
+.scrollContainer {
+  width: 100%;
+  overflow-y: auto;
+  height: 500px;
+}
+
+.roadmapHeader {
+  padding: 10px;
+  background-color: rgb(89, 134, 198);
+  text-align: left;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.roadmapTitle {
+  color: white;
+}
+
+.postContainer {
+  display: inline-flex;
+  padding: 20px;
+}
+
+.upvoteContainer {
+  display: inline-flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 5px;
+  width: 35px;
+  height: 45px;
+  justify-content: center;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+.upvote-button {
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+  outline: none;
+  align-self: center;
+}
+
+.upvoteCount {
+  color: black;
+  align-self: center;
+  font-size: 16px;
+}
+
+.fa-lg {
+  color: grey;
+}
+</style>
---
+++ src/views/Roadmap.vue
@@ -0,0 +1,97 @@
+
+<script setup>
+import Nav from '../components/Nav.vue'
+</script>
+
+<template>
+    <Nav/>
+    <div class="roadmapSpacer">
+    </div>
+    <div class="contentContainer">
+        <div class="contentInnerContainer">
+            <div class="roadmapContainer">
+        <div class="roadmapColumn">
+            <div class="roadmapHeader">
+            <h3>Planned</h3>
+        </div>
+            <div class="scrollContainer">
+                <div class="postList">
+                <p>link to post will appear here</p>
+                </div>
+            </div>
+        </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<style>
+
+.contentContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
+.contentInnerContainer {
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+}
+
+.roadmapSpacer {
+    padding: 50px;
+}
+
+.roadmapContainer {
+    display: grid;
+    justify-items: start;
+    align-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 10px;
+    grid-auto-rows: auto;
+    padding: 10px;
+    justify-content: center;
+    align-content: center;
+}
+
+.postList {
+    padding: 20px;
+}
+
+.roadmapColumn {
+    border: 1px solid gray;
+    background-color: E5E5E5;
+    border-radius: 10px;
+}
+.scrollContainer {
+    width: 100%;
+    overflow-y: auto;
+}
+
+.postList {
+    margin: 0px;
+    display: flex;
+    flex-direction: column;
+}
+
+.roadmapHeader {
+    padding: 20px;
+    font-weight: bold;
+    background-color: rgb(89, 134, 198);
+    color: white;
+    text-align: left;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    border: 1px solid rgb(89, 134, 198);
+}
+
+
+</style>
+
+<script>
+
+
+</script>
