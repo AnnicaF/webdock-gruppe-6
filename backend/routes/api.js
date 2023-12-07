626c3d16fa79bb054a31dcf8f41d4a432b342796
Annica <annif011098@gmail.com>
2023-12-07 12:36:09 +0100

    Update MVC structur for SSO

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const requests = require("../controller/requests");
+const auth = require("../controller/auth");
 
 const router = express.Router();
 
 
 router.get("/v1/request", requests.show);
-router.post("/v1/request", requests.create)
+router.post("/v1/request", requests.create);
+
+router.post("/v1/authenticate", auth.authentication);
 
 module.exports = router;
