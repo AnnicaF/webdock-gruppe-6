368e8ba5cc5c7f3762602c9adf10b9c253bf984d
seyyitsahin <mainn2@hotmail.com>
2023-12-14 11:17:27 +0100

    search can now also find bodytext as well

--- backend/controller/requests.js
+++ backend/controller/requests.js
@@ -20,9 +20,19 @@ exports.search = async (req, res) => {
 
     const requests = await Request.findAll({
       where: {
-        title: {
-          [Op.like]: `%${searchQuery}%`, // Allows for partial matching
-        },
+        // condition for search where the term is found either in title or the content of the posts
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: `%${searchQuery}%`, // Allows for partial matching
+            },
+          },
+          {
+            bodyText: {
+              [Op.like]: `%${searchQuery}%`,
+            },
+          },
+        ],
       },
     });
     return res.status(200).json(requests);
